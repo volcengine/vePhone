7f431c9ab2bf2443256b98febb04dbc2911627b4
jincan.2010 <jincan.2010@bytedance.com>
2023-11-14 18:21:58 +0800

    增加传感器相关示例代码

--- Quick Start/Android/app/src/main/java/com/example/sdkdemo/MainActivity.java
+++ Quick Start/Android/app/src/main/java/com/example/sdkdemo/MainActivity.java
@@ -9,6 +9,7 @@ import com.example.sdkdemo.feature.MessageChannelActivity;
 import com.example.sdkdemo.feature.MultiMediaStreamActivity;
 import com.example.sdkdemo.feature.PodControlServiceActivity;
 import com.example.sdkdemo.feature.RotationModeActivity;
+import com.example.sdkdemo.feature.SensorActivity;
 import com.example.sdkdemo.util.Feature;
 
 public class MainActivity extends BaseListActivity {
@@ -22,14 +23,15 @@ public class MainActivity extends BaseListActivity {
     protected void setupAdapter(ItemsHolder holder) {
         holder.addItem(R.string.audio, R.string.audio_desc, FeatureActivity.class, Feature.FEATURE_AUDIO);
         holder.addItem(R.string.camera, R.string.camera_desc, FeatureActivity.class, Feature.FEATURE_CAMERA);
-        holder.addItem(R.string.clarity, R.string.clarity_desc, ClarityServiceActivity.class, Feature.FEATURE_CAMERA);
-        holder.addItem(R.string.clipboard, R.string.clipboard_desc, ClipBoardServiceManagerActivity.class, Feature.FEATURE_CAMERA);
+        holder.addItem(R.string.clarity, R.string.clarity_desc, ClarityServiceActivity.class, -1);
+        holder.addItem(R.string.clipboard, R.string.clipboard_desc, ClipBoardServiceManagerActivity.class, -1);
         holder.addItem(R.string.file_exchange, R.string.file_exchange_desc, FeatureActivity.class, Feature.FEATURE_FILE_EXCHANGE);
         holder.addItem(R.string.location, R.string.location_desc, FeatureActivity.class, Feature.FEATURE_LOCATION);
-        holder.addItem(R.string.message_channel, R.string.message_channel_desc, MessageChannelActivity.class, Feature.FEATURE_CAMERA);
-        holder.addItem(R.string.pod_control, R.string.pod_control_desc, PodControlServiceActivity.class, Feature.FEATURE_CAMERA);
+        holder.addItem(R.string.message_channel, R.string.message_channel_desc, MessageChannelActivity.class, -1);
+        holder.addItem(R.string.pod_control, R.string.pod_control_desc, PodControlServiceActivity.class, -1);
         holder.addItem(R.string.rotation_mode, R.string.rotation_mode_desc, RotationModeActivity.class, Feature.FEATURE_ROTATION_MODE);
         holder.addItem(R.string.multi_media_stream, R.string.multi_media_stream_desc, MultiMediaStreamActivity.class, Feature.FEATURE_MULTI_MEDIA_STREAM);
+        holder.addItem(R.string.sensor, R.string.sensor_desc, SensorActivity.class, -1);
     }
 
     @Override
