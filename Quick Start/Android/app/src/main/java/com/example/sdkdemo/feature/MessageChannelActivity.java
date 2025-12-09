569fb0f7d213dfa1982093fba1b70eca3ab435d2
huanghao <huanghao.6@bytedance.com>
2025-12-09 17:30:21 +0800

    perf: android Demo优化messageChannel示例代理

--- Quick Start/Android/app/src/main/java/com/example/sdkdemo/feature/MessageChannelActivity.java
+++ Quick Start/Android/app/src/main/java/com/example/sdkdemo/feature/MessageChannelActivity.java
@@ -1,12 +1,13 @@
 package com.example.sdkdemo.feature;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,7 +22,12 @@ import com.volcengine.cloudphone.apiservice.IMessageChannel;
 import com.volcengine.phone.PhonePlayConfig;
 import com.volcengine.phone.VePhoneEngine;
 
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 该类用于展示与消息通道{@link IMessageChannel}相关的功能接口
@@ -30,10 +36,11 @@ import java.util.Map;
 public class MessageChannelActivity extends BasePlayActivity {
 
     private FrameLayout mContainer;
-    private IMessageChannel mMessageChannel;
     private SwitchCompat mSwShowOrHide;
     private LinearLayoutCompat mLlButtons;
     private Button mBtnAckMsg, mBtnUidAckMsg, mBtnTimeoutMsg, mBtnUidTimeoutMsg;
+    private TextView mTvOnlineCid;
+    private final MessageChannelController controller = new MessageChannelController();
 
 
     @Override
@@ -53,85 +60,61 @@ public class MessageChannelActivity extends BasePlayActivity {
         mBtnUidAckMsg = findViewById(R.id.btn_uid_ack_msg);
         mBtnTimeoutMsg = findViewById(R.id.btn_timeout_msg);
         mBtnUidTimeoutMsg = findViewById(R.id.btn_uid_timeout_msg);
+        mTvOnlineCid = findViewById(R.id.tv_online_cid);
 
         mSwShowOrHide.setOnCheckedChangeListener((buttonView, isChecked) -> {
             mLlButtons.setVisibility(isChecked ? View.VISIBLE : View.GONE);
         });
 
-        String channelUid = "com.bytedance.vemessagechannelprj.prj1";
         mBtnAckMsg.setOnClickListener(v -> {
-            if (mMessageChannel != null) {
-                /**
-                 * 发送回执消息到云端应用(当云端只有一个应用注册消息通道时使用)
-                 *
-                 * @param payload 发送内容，size：60KB
-                 * @param needAck 是否需要云端Ack回执
-                 * @return 消息实体
-                 */
-                IMessageChannel.IChannelMessage ackMsg =
-                        mMessageChannel.sendMessage("ackMsg", true);
-                Log.i(TAG, "ackMsg: " + ackMsg);
-            }
-            else {
-                Log.e(TAG, "mMessageChannel == null");
-            }
+            /*
+             * 发送回执消息到云端应用(当云端只有一个应用注册消息通道时使用)
+             *
+             * @param payload 发送内容，size：60KB
+             * @param needAck 是否需要云端Ack回执
+             * @return 消息实体
+             */
+            IMessageChannel.IChannelMessage msg = controller.sendMessage("msg", true);
+            Log.i(TAG, "msg: " + msg);
         });
         mBtnUidAckMsg.setOnClickListener(v -> {
-            if (mMessageChannel != null) {
-                /**
-                 * 发送回执消息到云端应用(当云端有多个应用注册消息通道时使用，需要指定目标用户ID，即应用包名)
-                 *
-                 * @param payload        发送内容，size：60KB
-                 * @param needAck        是否需要云端Ack回执
-                 * @param destChannelUid 目标用户消息通道ID
-                 * @return 消息实体
-                 */
-                IMessageChannel.IChannelMessage uidAckMsg =
-                        mMessageChannel.sendMessage("uidAckMsg", true, channelUid);
-                Log.i(TAG, "uidAckMsg: " + uidAckMsg);
-            }
-            else {
-                Log.e(TAG, "mMessageChannel == null");
-            }
+            /*
+             * 发送回执消息到云端应用(当云端有多个应用注册消息通道时使用，需要指定目标用户ID，即应用包名)
+             *
+             * @param payload        发送内容，size：60KB
+             * @param needAck        是否需要云端Ack回执
+             * @param destChannelUid 目标用户消息通道ID
+             * @return 消息实体
+             */
+            IMessageChannel.IChannelMessage msg = controller.sendMessage(controller.getTopOnlineChannelUid(), "msg", true);
+            Log.i(TAG, "msg: " + msg);
         });
         mBtnTimeoutMsg.setOnClickListener(v -> {
-            if (mMessageChannel != null) {
-                /**
-                 * 发送超时消息到云端应用(当云端只有一个应用注册消息通道时使用)
-                 *
-                 * @param payload 发送内容，size：60KB
-                 * @param timeout 消息超时时长，单位：ms，需要大于0；当小于等于0时，通过
-                 *                  {@link com.volcengine.cloudphone.apiservice.IMessageChannel.IMessageReceiver#onError(int, String)}
-                 *                  返回错误信息
-                 * @return 消息实体
-                 */
-                IMessageChannel.IChannelMessage timeoutMsg =
-                        mMessageChannel.sendMessage("timeoutMsg", 3000);
-                Log.i(TAG, "timeoutMsg: " + timeoutMsg);
-            }
-            else {
-                Log.e(TAG, "mMessageChannel == null");
-            }
+            /*
+             * 发送超时消息到云端应用(当云端只有一个应用注册消息通道时使用)
+             *
+             * @param payload 发送内容，size：60KB
+             * @param timeout 消息超时时长，单位：ms，需要大于0；当小于等于0时，通过
+             *                  {@link com.volcengine.cloudphone.apiservice.IMessageChannel.IMessageReceiver#onError(int, String)}
+             *                  返回错误信息
+             * @return 消息实体
+             */
+            IMessageChannel.IChannelMessage msg = controller.sendMessage("msg", 3000);
+            Log.i(TAG, "msg: " + msg);
         });
         mBtnUidTimeoutMsg.setOnClickListener(v -> {
-            if (mMessageChannel != null) {
-                /**
-                 * 发送超时消息到云端应用(当云端有多个应用注册消息通道时使用，需要指定目标用户ID，即应用包名)
-                 *
-                 * @param payload        发送内容，size：60KB
-                 * @param timeout        消息超时时长，单位：ms，需要大于0；当小于等于0时，通过
-                 *                         {@link com.volcengine.cloudphone.apiservice.IMessageChannel.IMessageReceiver#onError(int, String)}
-                 *                         返回错误信息
-                 * @param destChannelUid 目标用户消息通道ID
-                 * @return 消息实体
-                 */
-                IMessageChannel.IChannelMessage uidTimeoutMsg =
-                        mMessageChannel.sendMessage("uidTimeoutMsg", 3000, channelUid);
-                Log.i(TAG, "uidTimeoutMsg: " + uidTimeoutMsg);
-            }
-            else {
-                Log.e(TAG, "mMessageChannel == null");
-            }
+            /*
+             * 发送超时消息到云端应用(当云端有多个应用注册消息通道时使用，需要指定目标用户ID，即应用包名)
+             *
+             * @param payload        发送内容，size：60KB
+             * @param timeout        消息超时时长，单位：ms，需要大于0；当小于等于0时，通过
+             *                         {@link com.volcengine.cloudphone.apiservice.IMessageChannel.IMessageReceiver#onError(int, String)}
+             *                         返回错误信息
+             * @param destChannelUid 目标用户消息通道ID
+             * @return 消息实体
+             */
+            IMessageChannel.IChannelMessage msg = controller.sendMessage(controller.getTopOnlineChannelUid(), "msg", 3000);
+            Log.i(TAG, "msg: " + msg);
         });
     }
 
@@ -180,88 +163,165 @@ public class MessageChannelActivity extends BasePlayActivity {
     @Override
     public void onServiceInit(@NonNull Map<String, Object> extras) {
         super.onServiceInit(extras);
-        mMessageChannel = VePhoneEngine.getInstance().getMessageChannel();
-        if (mMessageChannel != null) {
-            /**
-             * 设置消息接收回调监听
-             *
-             * @param listener 消息接收回调监听器
-             */
-            mMessageChannel.setMessageListener(new IMessageChannel.IMessageReceiver() {
-                /**
-                 * 消息接收回调
-                 *
-                 * @param iChannelMessage 接收的消息实体
-                 */
-                @Override
-                public void onReceiveMessage(IMessageChannel.IChannelMessage iChannelMessage) {
-                    Log.i(TAG, "[onReceiveMessage] message: " + iChannelMessage);
-                    Toast.makeText(MessageChannelActivity.this, "[onReceiveMessage] message: " + iChannelMessage, Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onReceiveBinaryMessage(IMessageChannel.IChannelBinaryMessage iChannelBinaryMessage) {
-                    Log.d(TAG, "onReceiveBinaryMessage: ");
-                }
-
-                /**
-                 * 发送消息结果回调
-                 *
-                 * @param success 是否发送成功
-                 * @param messageId 消息ID
-                 */
-                @Override
-                public void onSentResult(boolean success, String messageId) {
-                    Log.i(TAG, "[onSentResult] success: " + success + ", messageId: " + messageId);
-                    Toast.makeText(MessageChannelActivity.this, "[onSentResult] success: " + success + ", messageId: " + messageId, Toast.LENGTH_SHORT).show();
-                }
-
-                /**
-                 * 已弃用，可忽略
-                 */
-                @Override
-                public void ready() {
-                    Log.i(TAG, "[ready]");
-                }
-
-                /**
-                 * 错误信息回调
-                 *
-                 * @param errorCode 错误码
-                 * @param errorMessage 错误信息
-                 */
-                @Override
-                public void onError(int errorCode, String errorMessage) {
-                    Log.i(TAG, "[onError] errorCode: " + errorCode + ", errorMessage: " + errorMessage);
-                    Toast.makeText(MessageChannelActivity.this, "[onError] errorCode: " + errorCode + ", errorMessage: " + errorMessage, Toast.LENGTH_SHORT).show();
-                }
-
-                /**
-                 * 云端游戏在线回调，建议在发送消息前监听该回调检查通道是否已连接
-                 *
-                 * @param channelUid 云端游戏的用户ID
-                 */
-                @Override
-                public void onRemoteOnline(String channelUid) {
-                    Log.i(TAG, "[onRemoteOnline] channelUid: " + channelUid);
-                    Toast.makeText(MessageChannelActivity.this, "[onRemoteOnline] channelUid: " + channelUid, Toast.LENGTH_SHORT).show();
-                }
-
-                /**
-                 * 云端游戏离线回调
-                 *
-                 * @param channelUid 云端游戏的用户ID
-                 */
-                @Override
-                public void onRemoteOffline(String channelUid) {
-                    Log.i(TAG, "[onRemoteOffline] channelUid: " + channelUid);
-                    Toast.makeText(MessageChannelActivity.this, "[onRemoteOffline] channelUid: " + channelUid, Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        else {
-            Log.e(TAG, "mMessageChannel == null");
-        }
+        // 初始化消息通道
+        controller.initMessageChannel();
     }
 
+
+    class MessageChannelController implements IMessageChannel.IMessageReceiver {
+        private static final String TAG = "MessageChannel";
+        private IMessageChannel messageChannel;
+        private final Map<String, Boolean> channelConnectState = new HashMap<>(4);
+
+        public void initMessageChannel() {
+            channelConnectState.clear();
+            messageChannel = VePhoneEngine.getInstance().getMessageChannel();
+            if (messageChannel != null) {
+                messageChannel.setMessageListener(this);
+            }
+        }
+
+        public IMessageChannel.IChannelMessage sendMessage(String payload, long timeoutInMillis) {
+            IMessageChannel mc = getOnlineChannel(null);
+            return mc == null ? null : mc.sendMessage(payload, timeoutInMillis);
+        }
+
+        public IMessageChannel.IChannelMessage sendMessage(String payload, boolean needAck) {
+            IMessageChannel mc = getOnlineChannel(null);
+            return mc == null ? null : mc.sendMessage(payload, needAck);
+        }
+
+        public IMessageChannel.IChannelMessage sendMessage(String channelUid, String payload, boolean needAck) {
+            IMessageChannel mc = getOnlineChannel(channelUid);
+            return mc == null ? null : mc.sendMessage(payload, needAck, channelUid);
+        }
+
+        public IMessageChannel.IChannelMessage sendMessage(String channelUid, String payload, long timeoutInMillis) {
+            IMessageChannel mc = getOnlineChannel(channelUid);
+            return mc == null ? null : mc.sendMessage(payload, timeoutInMillis, channelUid);
+        }
+
+        private @Nullable IMessageChannel getOnlineChannel(@Nullable String targetChannelUid) {
+            IMessageChannel mc = messageChannel;
+            if (mc == null) {
+                Log.d(TAG, "sendMessage: 当前messageChannel未初始化");
+                return null;
+            }
+            if (TextUtils.isEmpty(targetChannelUid)) {
+                if (!channelConnectState.containsValue(Boolean.TRUE)) {
+                    Log.d(TAG, "sendMessage: 当前无远端连接，无法发送消息");
+                    return null;
+                }
+            } else {
+                if (channelConnectState.get(targetChannelUid) != Boolean.TRUE) {
+                    Log.d(TAG, "sendMessage: 当前远端用户" + targetChannelUid + "未连接，无法发送消息");
+                    return null;
+                }
+            }
+            return mc;
+        }
+
+        public @NonNull Set<String> getAllChannelUids() {
+            return channelConnectState.keySet();
+        }
+
+        public @NonNull Set<String> getOnlineChannelUids() {
+            Set<String> set = new HashSet<>(4);
+            for (Map.Entry<String, Boolean> entry : channelConnectState.entrySet()) {
+                if (Boolean.TRUE.equals(entry.getValue())) {
+                    set.add(entry.getKey());
+                }
+            }
+            return set;
+        }
+
+        public @Nullable String getTopOnlineChannelUid() {
+            Set<String> set = getOnlineChannelUids();
+            Iterator<String> iterator = set.iterator();
+            return iterator.hasNext() ? iterator.next() : null;
+        }
+
+        /**
+         * 消息接收回调
+         *
+         * @param message 接收的消息实体
+         */
+        @Override
+        public void onReceiveMessage(IMessageChannel.IChannelMessage message) {
+            Log.d(TAG, "onReceiveMessage: message:" + message);
+        }
+
+        /**
+         * 消息接收回调
+         *
+         * @param message 接收的消息实体
+         */
+        @Override
+        public void onReceiveBinaryMessage(IMessageChannel.IChannelBinaryMessage message) {
+            Log.d(TAG, "onReceiveBinaryMessage: message:" + message);
+        }
+
+        /**
+         * 发送消息结果回调
+         *
+         * @param success 是否发送成功
+         * @param messageId 消息ID
+         */
+        @Override
+        public void onSentResult(boolean success, String messageId) {
+            Log.d(TAG, "onSentResult: success:" + success + ", messageId:" + messageId);
+        }
+
+        @Override
+        public void ready() {
+            Log.d(TAG, "ready");
+        }
+
+        /**
+         * 错误信息回调
+         *
+         * @param code 错误码
+         * @param msg 错误信息
+         */
+        @Override
+        public void onError(int code, String msg) {
+            Log.d(TAG, "onError: code:" + code + ", msg:" + msg);
+        }
+
+        /**
+         * 云端游戏在线回调，建议在发送消息前监听该回调检查通道是否已连接
+         *
+         * @param channelUid 云端游戏的用户ID
+         */
+        @Override
+        public void onRemoteOnline(String channelUid) {
+            Log.d(TAG, "onRemoteOnline: channelUid:" + channelUid);
+            boolean isFirstConnect = channelConnectState.isEmpty();
+            channelConnectState.put(channelUid, true);
+            if (isFirstConnect) {
+                onFirstConnectedInSession();
+            }
+            onOnlineStateChanged();
+        }
+
+        /**
+         * 云端游戏离线回调
+         *
+         * @param channelUid 云端游戏的用户ID
+         */
+        @Override
+        public void onRemoteOffline(String channelUid) {
+            Log.d(TAG, "onRemoteOffline: channelUid:" + channelUid);
+            channelConnectState.put(channelUid, false);
+            onOnlineStateChanged();
+        }
+
+        private void onFirstConnectedInSession() {
+            // 消息通道首次连接成功
+        }
+
+        private void onOnlineStateChanged() {
+            mTvOnlineCid.setText(MessageFormat.format("在线用户：\n{0}", getOnlineChannelUids()));
+        }
+    }
 }
